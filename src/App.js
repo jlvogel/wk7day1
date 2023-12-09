a037bfd6627a02b411aae47e40a07b11784b177e
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-12-09 05:47:07 -0500

    wk7day1 complete.  My head hurts a little haha but I followed along for the most part.  Components is an interesting concept

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // import logo from './logo.svg';
 import Header from "./components/Header";
 import Player from "./components/Player";
-import './App.css';
+import Board from "./components/Board";
+// import Square from "./components/Square";
+// import './App.css';
+import "./styles.css";
 
 function App() {
   return (
@@ -34,8 +37,8 @@ function App() {
       Let's make a custom prop called which Player and pass the appropriate player name
       */}
 
-      <Player whichPlayer='x'/>   {/* so is this a prop? */}
-      <Player whichPlayer='o'/>
+      <Player whichPlayer='X'/>   {/* so is this a prop? */}
+      <Player whichPlayer='O'/>
 
       {/*
       Now, we need to access these properties inside our Player component. Our player component is a child of App, and thus has access to props.  Don't worry if this doesn't make sense yet. We'll be returning to this concept over and over again and it'll start to come together.
@@ -45,6 +48,17 @@ function App() {
       NTS:
       I think I remember from previous learning on history of react that that classes used to be used and it helps understand some of the lingo.  Maybe...  I'm glad they're saying don't worry if it doesn't completely make sense yet
       */}
+
+      <Board/>
+      {/* <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/> */}
 
     </div>
   );
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,24 @@
-function Player() {
+/*
+COMMENTS COPIED FROM APP.JS--------------------------------------------
+
+Now, we need to access these properties inside our Player component. Our player component is a child of App, and thus has access to props.  Don't worry if this doesn't make sense yet. We'll be returning to this concept over and over again and it'll start to come together.
+*/
+
+/*
+NTS:
+I think I remember from previous learning on history of react that that classes used to be used and it helps understand some of the lingo.  Maybe...  I'm glad they're saying don't worry if it doesn't completely make sense yet
+
+----------------------------------------------------------------------------
+*/
+
+// Props are passed as object argument to our component, so we must define a parameter to receive it
+
+// conventionally the parameter is called props
+
+function Player(props) {
   return (
-    <div>
-      <h2>Player </h2>
+    <div className={props.whichPlayer}>
+      <h2>Player {props.whichPlayer} </h2>
       <h3>Wins: </h3>
     </div>
   )
